81f7c63cd5209a53fbbdc50237f3086c51a5877c
Javier E Rodriguez <138794005+TA-gatech@users.noreply.github.com>
2023-08-20 10:01:47 -0400

    Update create_user.py

--- 2FA/create_user.py
+++ 2FA/create_user.py
@@ -5,25 +5,25 @@ import crypt
 
 #checking whether program is running as a root or not.
 if os.getuid()!=0:
-    print "Please, run as root."             
+    print("Please, run as root.")
     sys.exit()
 
-uname=raw_input("Enter Username you want to add: ")
+uname=input("Enter Username you want to add: ")
 
 with open('/etc/shadow','r') as fp:         # Opening shadow file in read mode
     arr=[]
     for line in fp:                         # Enumerating through all the enteries in shadow file
         temp=line.split(':')
         if temp[0]==uname:                  # checking whether entered username exist or not
-            print ("The user already exist. Try deleting it first.")
+            print("The user already exist. Try deleting it first.")
             sys.exit()
 
-passwd=raw_input("Enter Password for the user: ")
-re_passwd=raw_input("Re-enter Password for the user: ")
+passwd=input("Enter Password for the user: ")
+re_passwd=input("Re-enter Password for the user: ")
 
 # just making sure you know what you are entering in password
 if passwd!=re_passwd:
-    print ("Paswword do not match")
+    print("Paswword do not match")
     sys.exit()
 
 rand1=os.urandom(6)
@@ -32,14 +32,14 @@ salt=str(b64encode(rand1).decode('utf-8'))  # generating salt, eight charachters
 hash=crypt.crypt(passwd,'$6$'+salt)         # generating hash
 line=uname+':'+hash+":17710:0:99999:7:::"
 file1=open("/etc/shadow","a+")              # Opening shadow file in append+ mode
-file1.write(line+'\n')			    # Making hash entry in the shadow file
+file1.write(line+'\n')                      # Making hash entry in the shadow file
 try:
-    os.mkdir("/home/"+uname)	            # Making home file for the user
+    os.mkdir("/home/"+uname)                # Making home file for the user
 except:
     print("Directory: /home/"+uname+" already exist")
-file2=open("/etc/passwd","a+")		    # Opening passwd file in append+ mode
+file2=open("/etc/passwd","a+")              # Opening passwd file in append+ mode
 
-count=1000				
+count=1000
 
 with open('/etc/passwd','r') as f:          # Opening passwd file in read mode
     arr1=[]
@@ -49,8 +49,8 @@ with open('/etc/passwd','r') as f:          # Opening passwd file in read mode
         while (int(temp1[3])>=count and int(temp1[3])<65534):
             count=int(temp1[3])+1           # assigning new uid = 1000+number of UIDs +1
 
-count=str(count)	
-str1=uname+':x:'+count+':'+count+':,,,:/home/'+uname+':/bin/bash' 
-file2.write(str1)                           # creating entry in passwd file for new user
+count=str(count)
+str1=uname+':x:'+count+':'+count+':,,,:/home/'+uname+':/bin/bash'
+file2.write(str1+'\n')                           # creating entry in passwd file for new user
 file2.close()
 file1.close()
