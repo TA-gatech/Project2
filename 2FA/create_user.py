5c1cfaa6cde0198ee7d36c8841f095dbc11f7cb0
Javier E Rodriguez <138794005+TA-gatech@users.noreply.github.com>
2023-08-29 22:44:36 -0400

    Update create_user.py

--- 2FA/create_user.py
+++ 2FA/create_user.py
@@ -7,6 +7,14 @@ from passlib.hash import sha512_crypt
 SHADOW_FILE = '/etc/shadow'
 PASSWD_FILE = '/etc/passwd'
 
+
+def check_root_privileges():
+    """Check if the program is running with root privileges."""
+    if os.getuid() != 0:
+        print("Please run as root.")
+        sys.exit()
+
+
 def get_valid_salt():
     while True:
         user_input = input("Enter an 8-character salt (lowercase letters and digits): ")
@@ -14,10 +22,11 @@ def get_valid_salt():
         if re.match(r"^[a-z0-9]{8}$", user_input):
             return user_input
         else:
-            print("Invalid salt. Please enter exactly 8 lowercase letters or digits.")
+            print("Invalid salt. Please enter exactly 8 lowercase letters and digits.")
             retry = input("Do you want to retry? (yes/no): ")
             if retry.lower() != "yes":
                 sys.exit("Exiting.")
+
 
 def user_exists(username):
     with open(SHADOW_FILE, 'r') as fp:
@@ -26,11 +35,12 @@ def user_exists(username):
                 return True
     return False
 
+
 def update_shadow_file(username, password_hash):
-    # Making hash entry in the shadow file
     shadow_line = f"{username}:{password_hash}:17710:0:99999:7:::"
     with open(SHADOW_FILE, 'a+') as shadow_file:
         shadow_file.write(shadow_line + '\n')
+
 
 def create_home_directory(username):
     try:
@@ -38,10 +48,8 @@ def create_home_directory(username):
     except FileExistsError:
         print("Directory: /home/" + username + " already exists")
 
+
 def update_passwd_file(username):
-    # Making user entry in the passwd file
-    
-    # Count is the default user ID number in our system
     count = 1000
 
     with open(PASSWD_FILE, 'r') as f:
@@ -56,6 +64,7 @@ def update_passwd_file(username):
     with open(PASSWD_FILE, 'a+') as passwd_file:
         passwd_file.write(passwd_line + '\n')
 
+
 def get_input(prompt, default=None):
     if default is not None:
         prompt += f" (or press Enter for {default}) : "
@@ -63,6 +72,7 @@ def get_input(prompt, default=None):
     if not response:
         return default
     return response
+
 
 def create_user(username):
     if user_exists(username):
@@ -72,7 +82,6 @@ def create_user(username):
     password = get_input("Enter Password for the user", "password")
     re_password = get_input("Re-enter Password for the user", "password")
 
-    # Just making sure you know what you are entering in password
     if password != re_password:
         print("Passwords do not match")
         sys.exit()
@@ -85,12 +94,11 @@ def create_user(username):
     create_home_directory(username)
 
 
-if __name__ == '__main__':
-    # Checking whether the program is running as a root or not.
-    if os.getuid() != 0:
-        print("Please, run as root.")
-        sys.exit()
-
+def main():
+    check_root_privileges()
     uname = get_input("Enter Username you want to add", "username")
     create_user(uname)
 
+
+if __name__ == '__main__':
+    main()
